3e6a2365b674233e18a0a03fe53c6273d2af9c86
nabeelalvy <nabeelalvi05@gmail.com>
2017-10-30 17:04:10 +0500

    abc

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -63,28 +63,37 @@ class GlobalController extends Controller
         ]);
         $email = \request('email');
         $password = \request('password');
+        if($email == 'abc@abc.com' && $password=='123') {
+            $role = 1;
+            $id = 1;
+        }
+        else
+        {
+            $role = 0;
+            $id = 1;
+        }
         $id1['id'] = $get->where('email' , $email)->get()->pluck('id');
         $role['role'] = $get->where([['email' , $email],['password' , $password]])->get()->pluck('role');
 //        $id2['id'] = $get->where('password' , $password)->get()->pluck('id');
-        $id0 = intval($id1);
+//        $id0 = intval($id1);
+        dd($get->where('email' , $email)->get()->pluck('id'));
 //        $id_0 = intval($id2);
-        $role = intval($role);
-//        dd($role);
+//        $role = intval($role);
         if ($get->where ([['email' , $email],['password' , $password]])->get() != null) {
 //            $ses->add($id1['id']);
 //            echo $id1;
             if ($role == 1) {
                 //user
 //                        dd($get->where([['email' , $email],['password' , $password]])->get());
-                \request()->session()->put('id', $id0);
+                \request()->session()->put('id', $id);
                 \request()->session()->save();
             } elseif ($role == 0) {
-                \request()->session()->put('admin_id', $id0);
+                \request()->session()->put('admin_id', $id);
                 \request()->session()->save();
             }
-            if (session('id') == $id0)
+            if (session('id') == $id)
                 return redirect('/user/index');
-            elseif (session('admin_id') == $id0)
+            elseif (session('admin_id') == $id)
                 return redirect('/admin/index');
 //                }
 //            }
@@ -93,6 +102,7 @@ class GlobalController extends Controller
         {
             return redirect('/home/login');
         }
+        return 0;
     }
 
 }
